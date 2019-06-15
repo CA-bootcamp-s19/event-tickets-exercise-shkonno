d5b00823c0f0de31e3986cf4a459c60bb00190d2
skonno <shkonno@users.noreply.github.com>
2019-06-15 18:52:54 +0900

    second submit

--- contracts/EventTicketsV2.sol
+++ contracts/EventTicketsV2.sol
@@ -8,7 +8,7 @@ contract EventTicketsV2 {
     /*
         Define an public owner variable. Set it to the creator of the contract when it is initialized.
     */
-    address payable owner;
+    address payable public owner;
     uint PRICE_TICKET = 100 wei;
 
     /*
